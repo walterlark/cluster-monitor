94fcaba7335fd8ca456e4c0fd6827c2cbfd123bd
Walter Blaurock <walterblaurock@gmail.com>
2011-05-02 01:26:37 -0400

    Added some scripts to start and stop a server using apache's load balancer.

---
+++ scripts/stop_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# == 1 ]; then
+	
+	# start the server
+	curl -s "http://lb.walterblaurock.com/balancer-manager/?lf=1&ls=0&wr=$1&rr=&dw=Disable&w=http%3A%2F%2F$1.walterblaurock.com&b=mycluster&nonce=9e162345-74ee-4f99-b077-5644c1708710" > /dev/null
+
+	# verify return value is 0 from curl
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+
+	# verify server has stopped
+
+	# return with exit status
+	exit 0
+	
+else
+	echo "error"
+fi
--- src/FusionServer.java
+++ src/FusionServer.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import clustermonitor.PerformanceMetrics;
 import clustermonitor.PhysicalHandle;
 
@@ -9,15 +16,15 @@ import clustermonitor.PhysicalHandle;
  */
 public class FusionServer implements PhysicalHandle {
 
-	private  String _serverName, _clusterName;
+	private String _serverName, _clusterName;
 	private long temp;
-	
+
 	public FusionServer(String serverName, String clusterName) {
 		_serverName = serverName;
 		_clusterName = clusterName;
 		temp = 0;
 	}
-	
+
 	@Override
 	public String getServerName() {
 		return _serverName;
@@ -30,13 +37,35 @@ public class FusionServer implements PhysicalHandle {
 
 	@Override
 	public boolean startServer() {
-		// TODO Auto-generated method stub
-		return true;
+		int rv = 1;
+		try {
+			System.out.println("Starting server!!!!!");
+			Process p = Runtime.getRuntime().exec(
+					"scripts/start_server.sh " + _serverName);
+			rv = p.waitFor();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return (rv == 0);
 	}
 
 	@Override
 	public void stopServer() {
-		// TODO Auto-generated method stub
+		try {
+			System.out.println("Stopping server!!!!!");
+			Process p = Runtime.getRuntime().exec(
+					"scripts/stop_server.sh " + _serverName);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -55,19 +84,19 @@ public class FusionServer implements PhysicalHandle {
 	@Override
 	public void getPerformanceMetrics(PerformanceMetrics performanceMetrics) {
 		// TODO Auto-generated method stub
-		//System.out.println("Getting performance metrics for server: " + _clusterName + "." + _serverName);
-		
+		// System.out.println("Getting performance metrics for server: " +
+		// _clusterName + "." + _serverName);
+
 		temp += 1;
-		
+
 		if (temp < 20) {
-		
+
 			performanceMetrics.setMetricValue("load", 2);
 		}
 		if (temp > 20 && temp < 60) {
 			performanceMetrics.setMetricValue("load", 0.4);
-			
-		}
-		else {
+
+		} else {
 			performanceMetrics.setMetricValue("load", 2);
 		}
 	}
